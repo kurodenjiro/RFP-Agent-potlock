2868773ccc6e39b9ef215e732b0e05fa09737a59
Nin Dang <nindang12345@gmail.com~>
2024-09-11 17:20:53 +0700

    feat:create_project

--- src/app/projects/create_project/page.tsx
+++ src/app/projects/create_project/page.tsx
@@ -7,8 +7,8 @@ const create_project = () =>{
             <Header/>
             <div className="flex justify-center items-center">
                 <div className=" w-full md:mt-20 mt-10 pb-20">
-                    <div className="bg-slate-200 w-full flex flex-col px-20 items-start justify-start">
-                        <div className="flex flex-col gap-5  items-center my-10 mx-5">
+                    <div className="bg-slate-200 w-full flex flex-col px-10 md:px-20 items-start justify-start">
+                        <div className="flex flex-col gap-5  items-left my-10 md:mx-5">
                             <span className="text-2xl text-left font-bold ">Create Project</span>
                             <div className="flex gap-1">
                                 <p className=" font-bold">Project &#62;</p>
@@ -16,26 +16,53 @@ const create_project = () =>{
                             </div>
                         </div>
                     </div> 
-                    <div className="flex flex-row justify-between px-20">
-                        <div className="flex flex-col w-[1100px] gap-5">
-                            <div className="w-100% h-full gap-5 flex flex-col justify-center items-center">
-                                <div className="flex">
-                                    <img width={120} src="/assets/icon/avatar.png" alt="" />
-                                    <img width={40} className="translate-x-[-40px] translate-y-[40px]" src="/assets/icon/camera.svg" alt="" />
+                    <div className="flex flex-col-reverse md:flex-row justify-between px-2 py-5 md:p-20">
+                        <div className="flex flex-col w-full md:w-[1100px] gap-5">
+                            <div className="py-5 md:hidden">
+                                <div className="flex flex-col mt-4">
+                                    <span className="font-bold">Category</span>
+                                    <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
+                                    <div className="flex justify-between px-1 py-2 border border-solid">
+                                        <span className="text-sm text-gray-400">Choose Category</span>
+                                        <img width={16} src="/assets/icon/arrow-down-gray.svg" alt="" />
+                                    </div>
                                 </div>
-                                <small className="relative left-[-20px] font-bold">Add member</small>
+                                <div className="flex flex-col mt-4">
+                                    <span className="font-bold">Title</span>
+                                    <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
+                                    <div className="flex justify-between px-1 py-2 border border-solid">
+                                        <span className="text-sm text-gray-400">Enter Title Here</span>
+                                        <img width={16} src="/assets/icon/arrow-down-gray.svg" alt="" />
+                                    </div>
+                                </div>
+                                <div className="flex flex-col mt-4">
+                                    <span className="font-bold">Summary</span>
+                                    <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
+                                    <div className="flex justify-between px-1 py-2 border border-solid">
+                                        <span className="text-sm text-gray-400">Enter Summary Here </span>
+                                        <img width={16} src="/assets/icon/arrow-down-gray.svg" alt="" />
+                                    </div>
+                                </div>
+
                             </div>
-                            <div className="flex flex-col">
+                            <div className="w-100% h-full gap-5 flex md:flex-col flex-row justify-start md:justify-center items-center">
+                                <div className="flex">
+                                    <img className="w-[56px] md:w-[120px]" src="/assets/icon/avatar.png" alt="" />
+                                    <img className="translate-x-[-20px] translate-y-[20px] md:translate-x-[-40px] md:translate-y-[40px] md:w-[40px] w-[19px]" src="/assets/icon/camera.svg" alt="" />
+                                </div>
+                                <small className="relative left-[-20px] text-base font-bold">Add member</small>
+                            </div>
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Project ID</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <input type="text"placeholder="Enter title here" className="text-sm px-1 py-2 focus outline-none border border-solid" />
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Project Name</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <input type="text"placeholder="Enter title here" className="text-sm px-1 py-2 focus outline-none border border-solid" />
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Category</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <div className="flex justify-between px-1 py-2 border border-solid">
@@ -43,21 +70,21 @@ const create_project = () =>{
                                     <img width={16} src="/assets/icon/arrow-down-gray.svg" alt="" />
                                 </div>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Why Do you consider yourself a public good</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <div className="flex items-start border border-solid h-[96px] px-1 py-2">
                                     <textarea placeholder="Enter response here." className="text-sm focus outline-none" />
                                 </div>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Description</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <div className="flex items-start border border-solid h-[96px] px-1 py-2">
                                     <textarea placeholder="Enter response here." className="text-sm focus outline-none" />
                                 </div>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col gap-2">
                                 <span className="font-bold">Project Name</span>
                                 <small className="text-gray-400">Lorem ipsum dolor sit amet consectetur. Orci purus condimentum aliquet volutpat tincidunt semper. Eu eget arcu sodales at.</small>
                                 <input type="text"placeholder="Enter title here" className="text-sm px-1 py-2 focus outline-none border border-solid" />
@@ -76,9 +103,10 @@ const create_project = () =>{
                             <div className="flex gap-10 justify-end items-center">
                                 <div className="text-gray-600">Discard Changes</div>
                                 <div className="flex flex-row border border-solid justify-center items-center border-black rounded-3xl p-2">
-                                    <div className="flex gap-2 px-3 border-r border-black h-full">
-                                        <img src="/assets/icon/ellipse.svg" alt="" />
-                                        <span>Create Project</span>
+                                    <div className="flex gap-2 px-3 border-r items-center border-black h-full">
+                                        <div className="w-[8px] h-[8px] bg-slate-400 rounded-full"></div>
+                                        <span className="hidden md:block">Create Project</span>
+                                        <span className="md:hidden">Submit draft</span>
                                     </div>
                                     <div className="px-2">
                                         <img src="/assets/icon/arrow-down-gray.svg"width={20} alt="" />
@@ -86,11 +114,11 @@ const create_project = () =>{
                                 </div>
                             </div>
                         </div>
-                        <div className="w-[500px]"> 
+                        <div className="w-full md:w-[500px]"> 
                             <div className="flex flex-col border-b border-solid">
-                                <span className="text-gray-600 font-bold mt-2">Author Details</span>
+                                <span className="text-gray-600 text-lg font-bold mt-2">Author Details</span>
                                 <div className="flex flex-col py-3 gap-2">
-                                    <span className="text-sm font-bold">
+                                    <span className="font-bold">
                                         Author
                                     </span>
                                     <div className="flex items-center">
@@ -100,20 +128,20 @@ const create_project = () =>{
                                 </div>
                             </div>
                             <div className="flex flex-col gap-4">
-                                <div>
-                                    <span className="mt-10 font-bold text-gray-500">
+                                <div className="flex flex-col">
+                                    <span className="py-[20px] font-bold text-gray-500">
                                         Smart Contract Address
                                     </span>
                                     <div className="flex flex-col gap-2">
                                         <span>Wallet Address</span>
-                                        <span className="text-xs text-gray-300">Enter the address that where your smart contract is saved.</span>
+                                        <span className="text-sm text-gray-300">Enter the address that where your smart contract is saved.</span>
                                         <div className="flex flex-col gap-2 p-2">
                                             <div className="flex justify-between"> 
-                                                <Link className="text-xs font-bold" href={''}>genadrop-contract.nftgen.near</Link>
+                                                <Link className="text-sm font-bold" href={''}>genadrop-contract.nftgen.near</Link>
                                                 <img src="/assets/icon/trash.svg" alt="" />
                                             </div>
                                             <div className="flex justify-between"> 
-                                                <Link className="text-xs font-bold" href={''}>genadrop-contract.nftgen.near</Link>
+                                                <Link className="text-sm font-bold" href={''}>genadrop-contract.nftgen.near</Link>
                                                 <img src="/assets/icon/trash.svg" alt="" />
                                             </div>
                                         </div>
@@ -129,14 +157,14 @@ const create_project = () =>{
                                         Open source repositories
                                     </span>
                                     <div className="flex flex-col gap-2">
-                                        <span className="text-xs text-gray-300">Enter the address that where your smart contract is saved.</span>
+                                        <span className="text-sm text-gray-300">Enter the address that where your smart contract is saved.</span>
                                         <div className="flex flex-col gap-2 p-2">
                                               <div className="flex justify-between"> 
-                                                <Link className="text-xs font-bold" href={''}>https://github.com/Testproduct/v1-core.git</Link>
+                                                <Link className="text-sm font-bold" href={''}>https://github.com/Testproduct/v1-core.git</Link>
                                                 <img src="/assets/icon/trash.svg" alt="" />
                                             </div>
                                             <div className="flex justify-between"> 
-                                                <Link className="text-xs font-bold" href={''}>https://github.com/Testproduct/v1-core.git</Link>
+                                                <Link className="text-sm font-bold" href={''}>https://github.com/Testproduct/v1-core.git</Link>
                                                 <img src="/assets/icon/trash.svg" alt="" />
                                             </div>
                                         </div>
